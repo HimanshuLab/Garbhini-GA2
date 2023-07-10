910e5bef486a7990e45d3e9b7aa32a9105729255
Veerendra Gadekar <gpveerendra09@gmail.com>
2023-07-10 09:50:47 +0200

    script for RF grid search

---
+++ 2a_hyperparameter_tuning_randomForest.R
@@ -0,0 +1,76 @@
+#!/usr/bin/env Rscript 
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# Veerendra P Gadekar 
+# Email: ic36871@imail.iitm.ac.in; gpveerendra09@gmail.com
+
+# script generate random forest models
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+library(openxlsx)
+library(data.table)
+require(ranger) #for Random Forest model
+library(randomforest)
+library(dplyr)
+library(tidyverse)
+require(xgboost) #for XG Boost model
+library(BlandAltmanLeh) #for bland-altman analysis
+library(reshape2)
+library(gridExtra)
+library(tidyverse)
+library(haven)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+seed_value <- 666
+seed_range <- 100:599
+
+datadir <- "./data/"
+#outdir <- "./results"
+log_ga_file <- "./data/extended_input_set/dbscan_train_log_ga.xlsx"
+log_ga = subset(read.xlsx(log_ga_file), select = -c(X1));                   
+train_file <- "./data/extended_input_set/dbscan_train_data_classic_log_sqrt.xlsx"
+train_data <- subset(read.xlsx(train_file),  select = -c(X1))
+train <- setDT(cbind(log_ga, train_data)); setnames(train, old = "x", new = "logGA")
+figures <- "./results/figures/"
+
+hyper_grid <- expand.grid(
+  mtry       = seq(3, 15, by = 1),
+  node_size  = seq(3, 10, by = 1),
+  sampe_size = c(.55, .632, .70, .80),
+  OOB_RMSE   = 0
+)
+
+RFmodel <- randomForest(
+  formula = logGA ~ .,
+  data    = train
+)
+
+pdf(paste(figures,"Out-Of-Bag_MSE_across_ntrees.pdf", sep = ""))
+plot(RFmodel, main = "Out-Of-Bag error plot Random Forest")
+abline(v = which.min(RFmodel$mse), lty = 2, col = "red")
+dev.off()
+
+model <- 
+ apply(hyper_grid, 1, 
+   function(y){      
+      ranger(
+        formula         = logGA ~ ., 
+        data            = train, 
+        num.trees       = which.min(RFmodel$mse),
+        mtry            = y["mtry"],
+        min.node.size   = y["node_size"],
+        sample.fraction = y["sampe_size"],
+        seed            = 123)
+    })
+
+hyper_grid$OOB_RMSE = unlist(lapply(model, function(x) x$prediction.error))
+hyper_grid = setDT(hyper_grid)[order(OOB_RMSE)]
+
+write.table(hyper_grid, paste(outdir,"hyper_grid_RF.tsv", sep = "/"), sep = "\t", row.names = F, quote = F)
+write.table(train, paste(datadir,"RF_XGB_train_data.tsv", sep = "/"), sep = "\t", row.names = F, quote = F)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+
