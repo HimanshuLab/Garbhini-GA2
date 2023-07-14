8821bc9bc4b5aff946adf79e5517439130287a81
Veerendra Gadekar <gpveerendra09@gmail.com>
2023-07-14 12:14:14 +0200

    update

--- 2a_hyperparameter_tuning_randomForest.R
+++ 2a_hyperparameter_tuning_randomForest.R
@@ -69,15 +69,20 @@ hyper_grid <- setDT(hyper_grid)[order(OOB_RMSE)]
 # values of mtry, min.node.size, sample.fraction taken from the results from the hyper grid
 # from the above step
 
+print("Selected Hyperparameters:\n")
+print("mtry: "hyper_grid$mtry[1]"\n")
+print("min.node.size: "hyper_grid$min.node.size[1]"\n")
+print("sample.fraction: "hyper_grid$sample.fraction[1]"\n")
+
 OOB_RMSE <- vector(mode = "numeric", length = 100)
 for (i in seq_along(OOB_RMSE)) {
   optimal_ranger <- ranger(
     formula         = logGA ~ ., 
     data            = train, 
     num.trees       = 100,
-    mtry            = 3,
-    min.node.size   = 10,
-    sample.fraction = .55,
+    mtry            = hyper_grid$mtry[1],
+    min.node.size   = hyper_grid$min.node.size[1],
+    sample.fraction = hyper_grid$sample.fraction[1],
     importance      = 'impurity'
   )
   OOB_RMSE[i] <- sqrt(optimal_ranger$prediction.error)
